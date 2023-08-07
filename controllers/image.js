671d9cf95ca79f2b9df92ce53037fae940497e23
Willy C <willyc@Willys-MBP.hsd1.ca.comcast.net>
2023-08-07 13:17:18 -0700

    fix fetch issue on Render

--- controllers/image.js
+++ controllers/image.js
@@ -1,9 +1,12 @@
-const handleApiCall = (req, res) => {
+import fetch from 'cross-fetch'; 
+
+const returnClarifaiRequestOptions = (imageUrl) => {
     const PAT = '777c777436b64ec59c69efc297a4c726';
     const USER_ID = 'willy77229';
     const APP_ID = 'smart-brain';
     // Change these to whatever model and image URL you want to use
     const MODEL_ID = 'face-detection';
+    const image_URL = imageUrl;
   
     const raw = JSON.stringify({
       user_app_id: {
@@ -14,7 +17,7 @@ const handleApiCall = (req, res) => {
         {
           data: {
             image: {
-              url: req.body.input,
+              url: image_URL,
             },
           },
         },
@@ -29,16 +32,19 @@ const handleApiCall = (req, res) => {
       },
       body: raw,
     };
-  
+    return requestOptions;
+  };
+
     // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
     // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
     // this will default to the latest version_id
-  
-    fetch(
+    
+    const handleApiCall = (req, res) => {
+      fetch(
       'https://api.clarifai.com/v2/models/' +
         MODEL_ID +
         '/outputs',
-      requestOptions
+      returnClarifaiRequestOptions(req.body.input)
     )
       .then(response => response.json())
       .then(data => {
@@ -48,7 +54,8 @@ const handleApiCall = (req, res) => {
         console.log('error', error);
         return res.status(400).json('unable to work with API');
       });
-  };
+    }
+  
 
  const handleImage = (req, res, db) => {
     const { id } = req.body;
